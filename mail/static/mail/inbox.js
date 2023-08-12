6c3db8013018566dc884be4a83e047298754087c
mateobordi1 <mategabriel01111@gmail.com>
2023-08-12 13:59:46 +0200

    abrir mail individual perfect

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -16,6 +16,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#email-individual').innerHTML= '';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -28,6 +29,8 @@ function load_mailbox(mailbox) {
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-individual').innerHTML= '';
+  
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -35,8 +38,6 @@ function load_mailbox(mailbox) {
   fetch(`/emails/${mailbox}`)
     .then(response => response.json())
     .then(emails => {
-    // Print emails
-    console.log(emails);
 
     const emailsView = document.querySelector('#emails-view');
 
@@ -44,15 +45,25 @@ function load_mailbox(mailbox) {
     emails.forEach(email => {
       const emailDiv = document.createElement('div');
       emailDiv.classList.add('mail');
+      const emailID = parseInt(`${email.id}`);
       emailDiv.innerHTML = `
         <p>sender: ${email.sender}</p>
         <p>subject: ${email.subject}</p>
         <p>timestamp: ${email.timestamp}</p>
         <hr>
       `;
+      // manejamos el color de fondo dependiendo de si fue leido o no
+      emailDiv.className = email.read ? "read" : "unread" ;
+
+      //agregamos un detector de cuando se haga click en el div
+      emailDiv.addEventListener("click", function(){
+        email_individual(`${email.id}`);
+      })
 
       // Agregar el emailDiv al final del contenido existente dentro de #emails-view
       emailsView.insertAdjacentElement('beforeend', emailDiv);
+
+
     });
     
   });
@@ -75,4 +86,42 @@ function send_email(event) {
       console.log(result);
   });
   window.location.reload();
+}
+
+function email_individual(email_id){
+
+  fetch(`/emails/${email_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: true
+    })
+  })
+
+  const emailIndividual = document.querySelector('#email-individual');
+
+
+  fetch(`/emails/${email_id}`)
+  .then(response => response.json())
+  .then(email => {
+    console.log(email)
+
+      //ocultamos el contenido de los otros divs
+    document.querySelector('#emails-view').style.display = 'none';
+    document.querySelector('#compose-view').style.display = 'none';
+    
+    //ponemos los datos del email dentro del div email-individual
+    const emailDivIndividual = document.createElement('div');
+    emailDivIndividual.classList.add('mailIndividual');
+    emailDivIndividual.innerHTML = `
+      <p> Sender : ${email.sender} </p>
+      <p> Recipents : ${email.recipients}</p>
+      <p> Subject: ${email.subject}</p>
+      <p> Timestamp: ${email.timestamp}</p>
+      <p> Body : ${email.body} </p>
+      <p> read : ${email.read} </p>
+      `
+    emailIndividual.innerHTML = '';
+    emailIndividual.appendChild( emailDivIndividual);
+  });
+
 }
